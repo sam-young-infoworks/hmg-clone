250657394e55e48b0e01dc6b52faf5c60d9e7243
Sam Young <sam.young@infoworks-tn.com>
2023-10-18 17:11:43 -0500

    update to use axios to fix fetch build error

--- _data/backpacks.js
+++ _data/backpacks.js
@@ -1,43 +1,35 @@
+const axios = require('axios');
+
 module.exports = async function () {
-  const result = await fetch(
-    'https://api.crystallize.com/infoworks/catalogue',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `
-          query ($language: String!, $path: String!) {
-            catalogue(language: $language, path: $path) {
-              children {
-                path
-                name
-                ... on Product {
-                  variants {
-                    name
-                    price
-                    images {
-                      url
-                      altText
-                      key
-                    }
+  return axios({
+    method: 'post',
+    url: 'https://api.crystallize.com/infoworks/catalogue',
+    data: {
+      query: `
+        query ($language: String!, $path: String!) {
+          catalogue(language: $language, path: $path) {
+            children {
+              path
+              name
+              ... on Product {
+                variants {
+                  name
+                  price
+                  images {
+                    url
+                    altText
+                    key
                   }
                 }
               }
             }
           }
-          `,
-        variables: {
-          language: 'en',
-          path: `/backpacks`,
-        },
-      }),
-    },
-  ).then((res) => res.json());
-
-  // console.log(result);
-  const backpacks = result?.data?.catalogue?.children ?? [];
-  // console.log(backpacks);
-  return backpacks;
+        }
+        `,
+      variables: {
+        language: 'en',
+        path: `/backpacks`,
+      },
+    }
+  }).then(res => res.data?.data?.catalogue?.children ?? []);  
 };
--- _data/tents.js
+++ _data/tents.js
@@ -1,40 +1,35 @@
+const axios = require('axios');
+
 module.exports = async function () {
-  const result = await fetch(
-    'https://api.crystallize.com/infoworks/catalogue',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `
-          query ($language: String!, $path: String!) {
-            catalogue(language: $language, path: $path) {
-              children {
-                path
-                name
-                ... on Product {
-                  variants {
-                    name
-                    price
-                    images {
-                      url
-                      altText
-                      key
-                    }
+  return axios({
+    method: 'post',
+    url: 'https://api.crystallize.com/infoworks/catalogue',
+    data: {
+      query: `
+        query ($language: String!, $path: String!) {
+          catalogue(language: $language, path: $path) {
+            children {
+              path
+              name
+              ... on Product {
+                variants {
+                  name
+                  price
+                  images {
+                    url
+                    altText
+                    key
                   }
                 }
               }
             }
           }
-          `,
-        variables: {
-          language: 'en',
-          path: `/tents`,
-        },
-      }),
-    },
-  ).then((res) => res.json());
-
-  return result?.data?.catalogue?.children ?? [];
+        }
+        `,
+      variables: {
+        language: 'en',
+        path: `/tents`,
+      },
+    }
+  }).then(res => res.data?.data?.catalogue?.children ?? []);
 };
